5b973b340b9616d2402db2538f79b8b09fbeec11
Federico Melo Barrero <f.melo@uniandes.edu.co>
2023-09-15 22:45:31 -0500

    Botón moradito

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
 import React from 'react';
-import './css/App.css';
-import Banner from './components/Header';
-import Footer from './components/Footer';
+import PurpleButton from './components/PurpleButton';
+
 
 function App() {
-  return (
-    <div className="App">
-      <Banner />
-
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-
-      <Footer />
-    </div>
-  );
+    return (
+        <div className="App">
+            <PurpleButton text="Alquilar" />
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './css/index.css';
+import './styles/index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './defaults/serviceWorkerRegistration';
-import reportWebVitals from './defaults/reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
