e9e15a24cc4b170c1ee88c42faa35a61ab9baab3
Federico Melo Barrero <f.melo@uniandes.edu.co>
2023-09-16 09:53:28 -0500

    Login Button

--- src/components/LoginButton.jsx
+++ src/components/LoginButton.jsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
+import { COLORS } from '../styles/colors';
 
 export default function LoginButton({ text }) {
     return <Button
         style={{
-            borderRadius: 13,
-            padding: "10px 40px",
-            backgroundColor: "#7724BF",
+            borderRadius: 20,
+            padding: "5px 20px",
+            backgroundColor: COLORS.primary,
             fontSize: "18px",
             textTransform: "none",
         }}
