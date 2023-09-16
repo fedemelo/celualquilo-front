88baf3e0d245d9b6e276b68c15843365f141ad80
Federico Melo Barrero <f.melo@uniandes.edu.co>
2023-09-16 09:09:39 -0500

    SearchBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import PurpleButton from './components/PurpleButton';
+import SearchBar from './components/SearchBar';
 
 
 function App() {
     return (
         <div className="App">
+            <SearchBar/>
             <PurpleButton text="Alquilar" />
         </div>
     );
---
+++ src/styles/colors.jsx
@@ -0,0 +1,10 @@
+/* Color palette, taken directly from the Figma file */
+
+export const COLORS = {
+    primary: '#9E30FF',
+    dark: '#280C40',
+    secondary: '#4F1880',
+    other: '#4F1880',
+    other2: '#8F2CE6',
+    lila: '#E6CAFF',
+  }
