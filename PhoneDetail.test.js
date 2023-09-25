3ed63f36fdec475065e0cfa806b8089233db1f59
Federico Melo Barrero <f.melo@uniandes.edu.co>
2023-09-24 22:48:56 -0500

    Tests fixed

--- PhoneDetail.test.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';import { render, screen } from '@testing-library/react';import PhoneDetail from '../components/PhoneDetail';import '@testing-library/jest-dom/extend-expect';
-
-test('renders Alquilar', () => {{render(<PhoneDetail/>);const searchedText = screen.getByText(/Alquilar/i);expect(searchedText).toBeInTheDocument();}});
-
-test('renders Reseñas de otros usuarios', () => {{render(<PhoneDetail/>);const searchedText = screen.getByText(/Reseñas de otros usuarios/i);expect(searchedText).toBeInTheDocument();}});
